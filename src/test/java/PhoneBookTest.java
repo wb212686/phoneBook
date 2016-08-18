db2f5f94ee83c857fa802408b2ae852618573e23
Dmitrij Ivanov <wb212686@gmail.com>
2016-08-19 00:22:30 +0300

    add test

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -10,5 +10,7 @@ public class PhoneBookTest {
     @Test
     public void getPhonesByNameTest() throws IOException {
         Assert.assertEquals(PhoneBook.getPhonesByName("Иванов И.И."), "1.+8 800 2000 500" + "\n" + "2.+8 800 200 600"+ "\n");
+        Assert.assertEquals(PhoneBook.getPhonesByName("Абросимов С.А."), "1.+8 800 20 500" + "\n"  + "2.+8 800 20000 600" + "\n");
+        Assert.assertEquals(PhoneBook.getPhonesByName("Поттер Г.И."), "такого ФИО в БД нет");
     }
 }
