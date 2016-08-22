17bb16d668f603487b60c9c12b0f5a7b73295a5a
wb212686 <wb212686@gmail.com>
2016-08-23 00:43:23 +0300

    refactoring PhoneBookTest

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 public class PhoneBookTest {
     @Test
     public void getPhonesByNameTest() throws IOException {
-        Assert.assertEquals(PhoneBook.getPhonesByName("Иванов И.И."), "1.+8 800 2000 500" + "\n" + "2.+8 800 200 600"+ "\n");
-        Assert.assertEquals(PhoneBook.getPhonesByName("Абросимов С.А."), "1.+8 800 20 500" + "\n"  + "2.+8 800 20000 600" + "\n");
-        Assert.assertEquals(PhoneBook.getPhonesByName("Поттер Г.И."), "такого ФИО в БД нет");
+        Assert.assertEquals(PhoneBook.getPhonesByName("Иванов И.И.").toString(), "1.+8 800 2000 500" + "\n" + "2.+8 800 200 600"+ "\n");
+        Assert.assertEquals(PhoneBook.getPhonesByName("Абросимов С.А.").toString(), "1.+8 800 20 500" + "\n"  + "2.+8 800 20000 600" + "\n");
+        Assert.assertEquals(PhoneBook.getPhonesByName("Поттер Г.И.").toString(), "такого ФИО в БД нет");
     }
 }
